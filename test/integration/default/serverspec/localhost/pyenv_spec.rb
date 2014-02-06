f286b95c9ac28212764ff87571e73ffd9481b6b0
Kei YAMAZAKI <daydream.yamazaki@gmail.com>
2014-02-06 14:55:07 +0900

    Update pip version in serverspec

--- test/integration/default/serverspec/localhost/pyenv_spec.rb
+++ test/integration/default/serverspec/localhost/pyenv_spec.rb
@@ -5,7 +5,7 @@ describe command('/usr/local/pyenv/shims/python -V') do
 end
 
 describe command('/usr/local/pyenv/shims/pip -V') do
-  it { should return_stdout 'pip 1.5 from /usr/local/pyenv/versions/2.7.6/lib/python2.7/site-packages/pip-1.5-py2.7.egg (python 2.7)' }
+  it { should return_stdout 'pip 1.5.2 from /usr/local/pyenv/versions/2.7.6/lib/python2.7/site-packages/pip-1.5.2-py2.7.egg (python 2.7)' }
 end
 
 describe file('/usr/local/pyenv/shims/python') do
