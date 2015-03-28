9ac4cba9432a154f4df580ea3e3e9349ccef8825
Marat Komarov <mac.komarov@gmail.com>
2015-03-28 15:16:50 +0200

    Updated python to 2.7.9

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
 default['pyenv']['root_path']     = '/usr/local/pyenv'
-default['pyenv']['pythons']       = %w(2.7.6)
-default['pyenv']['global']        = '2.7.6'
-default['pyenv']['pip']['2.7.6']  = []
+default['pyenv']['pythons']       = %w(2.7.9)
+default['pyenv']['global']        = '2.7.9'
+default['pyenv']['pip']['2.7.9']  = []
